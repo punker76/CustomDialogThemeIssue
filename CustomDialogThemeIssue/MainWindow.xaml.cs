2a6a314b7a04883a4c7b81e2ddcb659a1ca33f20
sarat12 <taras.if1@gmail.com>
2017-08-18 21:07:16 +0300

    Initial

---
+++ CustomDialogThemeIssue/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using MahApps.Metro;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace CustomDialogThemeIssue
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ShowClick(object sender, RoutedEventArgs e)
+        {
+            var dlg = (CustomDialog) Resources["CustomDialogTest"];
+
+            DialogCoordinator.Instance.ShowMetroDialogAsync(this, dlg);
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            var dlg = (CustomDialog)Resources["CustomDialogTest"];
+
+            DialogCoordinator.Instance.HideMetroDialogAsync(this, dlg);
+        }
+
+        private void LightThemeChosen(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.ChangeAppTheme(Application.Current, "BaseLight");
+        }
+
+        private void DarkThemeChosen(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.ChangeAppTheme(Application.Current, "BaseDark");
+        }
+    }
+}
